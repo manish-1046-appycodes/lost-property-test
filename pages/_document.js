554c8fed5e232188eea45b6d686a6a36c34cf4e8
Manish Sharma <manish@appycodes.com>
2023-04-14 12:09:24 +0530

    fixed mailchimp automatic popup

--- pages/_document.js
+++ pages/_document.js
@@ -33,18 +33,19 @@ class MyDocument extends Document {
             type="text/javascript"
             id="mcjs"
             dangerouslySetInnerHTML={{
-              __html: `  !(function (c, h, i, m, p) {
-                    (m = c.createElement(h)),
-                      (p = c.getElementsByTagName(h)[0]),
-                      (m.async = 1),
-                      (m.src = i),
-                      p.parentNode.insertBefore(m, p);
-                  })(
-                    document,
-                    "script",
-                    "https://chimpstatic.com/mcjs-connected/js/users/5a1c502d9787103942bfc76a5/7d3683af1b429d8f42b9fda83.js"
-                  );
-                }, 1000);`,
+              __html: `setTimeout(() => {
+                !(function (c, h, i, m, p) {
+                  (m = c.createElement(h)),
+                    (p = c.getElementsByTagName(h)[0]),
+                    (m.async = 1),
+                    (m.src = i),
+                    p.parentNode.insertBefore(m, p);
+                })(
+                  document,
+                  "script",
+                  "https://chimpstatic.com/mcjs-connected/js/users/5a1c502d9787103942bfc76a5/7d3683af1b429d8f42b9fda83.js"
+                );
+              }, 1000);`,
             }}
           />
 
