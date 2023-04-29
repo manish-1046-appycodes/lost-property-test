e702f8dd45c2df6e2e05acd8207214dfc6b27bde
Manish Sharma <manish@appycodes.com>
2023-04-29 10:13:13 +0530

    fix:integrating modified mailchimp popup

--- pages/_document.js
+++ pages/_document.js
@@ -10,21 +10,37 @@ class MyDocument extends Document {
     return (
       <Html lang="en-GB">
         <Head>
-           <script
+          {/* <script
             defer
             async
             type="text/javascript"
             src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"
-          /> 
+          /> */}
 
           {/* mailchimp script */}
-          <script
+          {/* <script
             type="text/javascript"
             id="mcjs"
             dangerouslySetInnerHTML={{
-              __html: `setTimeout(()=>{!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/e531d17fe22bad56eb6bd5377/${process.env.NEXT_PUBLIC_MAILCHIMP_ID}.js");},5000);`,
+              __html: `!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/e531d17fe22bad56eb6bd5377/${process.env.NEXT_PUBLIC_MAILCHIMP_ID}.js");`,
             }}
-          />
+          /> */}
+
+          <script id="mcjs">
+            {
+              !(function (c, h, i, m, p) {
+                (m = c.createElement(h)),
+                  (p = c.getElementsByTagName(h)[0]),
+                  (m.async = 1),
+                  (m.src = i),
+                  p.parentNode.insertBefore(m, p);
+              })(
+                document,
+                "script",
+                "https://chimpstatic.com/mcjs-connected/js/users/5a1c502d9787103942bfc76a5/a15b3ae73e898f7ad1c2167dc.js"
+              )
+            }
+          </script>
         </Head>
         <body>
           <Main />
